ff56ad92466bde5422c19df81a59bcc5c4a9882e
Ashutosh Verma <145222383+Ashutosh-v1@users.noreply.github.com>
2026-02-10 22:56:29 +0530

    Setup of AI Model to Generate Trip || using openRouter.ai to integrate ai model

---
+++ app/create-new-trip/_components/EmptyBoxState.tsx
@@ -0,0 +1,25 @@
+import { suggestions } from '@/app/_components/Hero'
+import React from 'react'
+
+function EmptyBoxState() {
+  return (
+    <div className='mt-7'>
+      <h2 className='font-bold text-3xl text-center'>Start Planning new <strong className='text-primary'>Trip</strong> using AI</h2>
+      <p className='text-center text-gray-400 mt-2'>Discover personalized travel itineraries tailored to your preferences and plan your dream vacation effortlessly with power of AI</p>
+    
+            <div className='flex flex-col gap-5'>
+                {suggestions.map((suggestions, index) => (
+                    <div key={index} className='flex items-center gap-2 border rounded-full p-2 shadow-sm cursor-pointer hover:bg-primary hover:text-white'>
+                        {suggestions.icon}
+                        <h2 className='text-sm'>{suggestions.title}</h2>
+                    </div>
+                ))}
+            </div>
+    
+    </div>
+  )
+}
+
+export default EmptyBoxState
+
+//1.58.24
